2489a2f9476abdd94c7eb17ae96a354ee8f2302f
Artyom Khamzin <akhamzin90@yandex.ru>
2017-01-30 03:29:51 -0800

    Add new fields in User model and views, add bootstrap for styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  # number_regex = ((8|\+7)-?)?\(?\d{3}\)?-?\d{1}-?\d{1}-?\d{1}-?\d{1}-?\d{1}-?\d{1}-?\d{1}
+  # validates :phone_number,  with: number_regex
+  validates :first_name, presence: true
+  validates :middle_name, presence: true
+  validates :last_name, presence: true
+  validates :company_name, presence: true
+  validates :description, presence: true
 end
