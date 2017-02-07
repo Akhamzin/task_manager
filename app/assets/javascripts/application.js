c7a97b3bd469334a0eed0f99bdd990940f13da9d
Artyom Khamzin <akhamzin90@yandex.ru>
2017-02-07 10:43:50 +0300

    Delete bootstrap require from application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
